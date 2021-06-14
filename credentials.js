6c958461fc89cdf1e57ab3a278f5454c36d4569b
mohan.singh1385664 <mohan.singh1385664@gmail.com>
2021-06-14 15:09:49 +0530

    Deleted creds

--- credentials.js
+++
@@ -1,4 +0,0 @@
-export const CLIENT_ID = '41921681602-adcamvpcjgt9ub8vju3u3ibv431fr8k1.apps.googleusercontent.com';
-export const API_KEY = 'AIzaSyA3R_MDh10LGIopyBb_HrpX3HYlXzEYfvA';
-export const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
-export const SCOPES = 'https://www.googleapis.com/auth/calendar.events';
